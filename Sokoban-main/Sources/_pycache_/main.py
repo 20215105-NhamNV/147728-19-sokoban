7217ce6c916d700b01f8fbb40ddd60cd5b645cc6
Nguyen Van Nham <nhampmq@gmail.com>
2024-06-07 22:40:05 +0700

    main running sokoban

--- Sokoban-main/Sources/_pycache_/main.py
+++ Sokoban-main/Sources/_pycache_/main.py
@@ -110,3 +110,156 @@ def renderMap(board):
 			if board[i][j] == '@':
 				screen.blit(player, (j * 32 + indent, i * 32 + 250))
 
+
+mapNumber = 0
+algorithm = "Breadth First Search"
+sceneState = "init"
+loading = False
+
+def sokoban():
+	running = True
+	global sceneState
+	global loading
+	global algorithm
+	global list_board
+	global mapNumber
+	stateLenght = 0
+	currentState = 0
+	found = True
+
+	while running:
+		screen.blit(init_background, (0, 0))
+		if sceneState == "init":
+			initGame(maps[mapNumber])
+		if sceneState == "executing":
+			list_check_point = check_points[mapNumber]
+			if algorithm == "Breadth First Search":
+				print("BFS")
+				list_board = bfs.BFS_search(maps[mapNumber], list_check_point)
+			else:
+				print("AStar")
+				list_board = astar.AStart_Search(maps[mapNumber], list_check_point)
+			if len(list_board) > 0:
+				sceneState = "playing"
+				stateLenght = len(list_board[0])
+				currentState = 0
+			else:
+				sceneState = "end"
+				found = False
+		if sceneState == "loading":
+			loadingGame()
+			sceneState = "executing"
+		if sceneState == "end":
+			if found:
+				foundGame(list_board[0][stateLenght - 1])
+			else:
+				notfoundGame()
+		if sceneState == "playing":
+			clock.tick(2)
+			renderMap(list_board[0][currentState])
+			currentState = currentState + 1
+			if currentState == stateLenght:
+				sceneState = "end"
+				found = True
+		#Check event when you press key board
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.KEYDOWN:
+				
+				#Press arrow key board to change level map
+				if event.key == pygame.K_RIGHT and sceneState == "init":
+					if mapNumber < len(maps) - 1:
+						mapNumber = mapNumber + 1
+				if event.key == pygame.K_LEFT and sceneState == "init":
+					if mapNumber > 0:
+						mapNumber = mapNumber - 1
+				#Press ENTER key board to select level map and algorithm
+				if event.key == pygame.K_RETURN:
+					if sceneState == "init":
+						sceneState = "loading"
+					if sceneState == "end":
+						sceneState = "init"
+				#Press SPACE key board to switch algorithm
+				if event.key == pygame.K_SPACE and sceneState == "init":
+					if algorithm == "Breadth First Search":
+						algorithm = "A Star Search"
+					else:
+						algorithm = "Breadth First Search"
+		pygame.display.flip()
+	pygame.quit()
+
+def initGame(map):
+	titleSize = pygame.font.Font('gameFont.ttf', 60)
+	titleText = titleSize.render('Among-koban', True, WHITE)
+	titleRect = titleText.get_rect(center=(320, 80))
+	screen.blit(titleText, titleRect)
+
+	desSize = pygame.font.Font('gameFont.ttf', 20)
+	desText = desSize.render('Now, select your map!!!', True, WHITE)
+	desRect = desText.get_rect(center=(320, 140))
+	screen.blit(desText, desRect)
+
+	mapSize = pygame.font.Font('gameFont.ttf', 30)
+	mapText = mapSize.render("Lv." + str(mapNumber + 1), True, WHITE)
+	mapRect = mapText.get_rect(center=(320, 200))
+	screen.blit(mapText, mapRect)
+
+	screen.blit(arrow_left, (246, 188))
+	screen.blit(arrow_right, (370, 188))
+
+	algorithmSize = pygame.font.Font('gameFont.ttf', 30)
+	algorithmText = algorithmSize.render(str(algorithm), True, WHITE)
+	algorithmRect = algorithmText.get_rect(center=(320, 600))
+	screen.blit(algorithmText, algorithmRect)
+	renderMap(map)
+
+#DISPLAY LOADING SCENE
+def loadingGame():
+	screen.blit(loading_background, (0, 0))
+
+	fontLoading_1 = pygame.font.Font('gameFont.ttf', 40)
+	text_1 = fontLoading_1.render('SHHHHHHH!', True, WHITE)
+	text_rect_1 = text_1.get_rect(center=(320, 60))
+	screen.blit(text_1, text_rect_1)
+
+	fontLoading_2 = pygame.font.Font('gameFont.ttf', 20)
+	text_2 = fontLoading_2.render('The problem is being solved, stay right there!', True, WHITE)
+	text_rect_2 = text_2.get_rect(center=(320, 100))
+	screen.blit(text_2, text_rect_2)
+
+def foundGame(map):
+	screen.blit(found_background, (0, 0))
+
+	font_1 = pygame.font.Font('gameFont.ttf', 30)
+	text_1 = font_1.render('Yeah! The problem is solved!!!', True, WHITE)
+	text_rect_1 = text_1.get_rect(center=(320, 100))
+	screen.blit(text_1, text_rect_1)
+
+	font_2 = pygame.font.Font('gameFont.ttf', 20)
+	text_2 = font_2.render('Press Enter to continue.', True, WHITE)
+	text_rect_2 = text_2.get_rect(center=(320, 600))
+	screen.blit(text_2, text_rect_2)
+
+	renderMap(map)
+
+def notfoundGame():
+	screen.blit(notfound_background, (0, 0))
+
+	font_1 = pygame.font.Font('gameFont.ttf', 40)
+	text_1 = font_1.render('Oh no, I tried my best :(', True, WHITE)
+	text_rect_1 = text_1.get_rect(center=(320, 100))
+	screen.blit(text_1, text_rect_1)
+
+	font_2 = pygame.font.Font('gameFont.ttf', 20)
+	text_2 = font_2.render('Press Enter to continue.', True, WHITE)
+	text_rect_2 = text_2.get_rect(center=(320, 600))
+	screen.blit(text_2, text_rect_2)
+
+	
+
+def main():
+	sokoban()
+
+if __name__ == "__main__":
+	main()
