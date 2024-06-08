76eae2d4a43d4ff4a96fb7d7fd56f65526538c08
huyhust <129529920+huyhust@users.noreply.github.com>
2024-06-08 08:11:25 +0700

    Update bfs.py

--- Sokoban-main/Sources/_pycache_/bfs.py
+++ Sokoban-main/Sources/_pycache_/bfs.py
@@ -22,3 +22,41 @@ def BFS_search(board, list_check_point):
         print("State visited : {}".format(len(list_state)))
         print("State in queue : {}".format(len(list_visit)))
         '''
+
+        ''' GET LIST POSITION THAT PLAYER CAN MOVE TO '''
+        list_can_move = spf.get_next_pos(now_state.board, cur_pos)
+        ''' MAKE NEW STATES FROM LIST CAN MOVE '''
+        for next_pos in list_can_move:
+            ''' MAKE NEW BOARD '''
+            new_board = spf.move(now_state.board, next_pos, cur_pos, list_check_point)
+            ''' IF THIS BOARD DON'T HAVE IN LIST BEFORE --> SKIP THE STATE '''
+            if spf.is_board_exist(new_board, list_state):
+                continue
+            ''' IF ONE OR MORE BOXES ARE STUCK IN THE CORNER --> SKIP THE STATE '''
+            if spf.is_board_can_not_win(new_board, list_check_point):
+                continue
+            ''' IF ALL BOXES ARE STUCK --> SKIP THE STATE '''
+            if spf.is_all_boxes_stuck(new_board, list_check_point):
+                continue
+
+            ''' MAKE NEW STATE '''
+            new_state = spf.state(new_board, now_state, list_check_point)
+            ''' CHECK WHETHER THE NEW STATE IS GOAL OR NOT '''
+            if spf.check_win(new_board, list_check_point):
+                print("Found win")
+                return (new_state.get_line(), len(list_state))
+            
+            ''' APPEND NEW STATE TO VISITED LIST AND TRAVERSED LIST '''
+            list_state.append(new_state)
+            list_visit.append(new_state)
+
+            ''' COMPUTE THE TIMEOUT '''
+            end_time = time.time()
+            if end_time - start_time > spf.TIME_OUT:
+                return []
+        end_time = time.time()
+        if end_time - start_time > spf.TIME_OUT:
+            return []
+    ''' SOLUTION NOT FOUND '''
+    print("Not Found")
+    return []
