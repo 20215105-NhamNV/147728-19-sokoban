20ed818702133a183dd0388c3c5b126e93293d8e
unknown <phamdinhhai1672003@gmail.com>
2024-06-08 12:44:26 +0700

    Update BFS

--- Sokoban-main/Sources/bfs.py
+++ Sokoban-main/Sources/bfs.py
@@ -14,49 +14,43 @@ def BFS_search(board, list_check_point):
         
         now_state = list_visit.pop(0)
         cur_pos = spf.find_position_player(now_state.board)
-       
-        time.sleep(1)
+
+       ''' 
+       time.sleep(1)
         clear = lambda: os.system('cls')
         clear()
         print_matrix(now_state.board)
         print("State visited : {}".format(len(list_state)))
         print("State in queue : {}".format(len(list_visit)))
+        '''
         
 
-        ''' GET LIST POSITION THAT PLAYER CAN MOVE TO '''
         list_can_move = spf.get_next_pos(now_state.board, cur_pos)
-        ''' MAKE NEW STATES FROM LIST CAN MOVE '''
         for next_pos in list_can_move:
             ''' MAKE NEW BOARD '''
             new_board = spf.move(now_state.board, next_pos, cur_pos, list_check_point)
-            ''' IF THIS BOARD DON'T HAVE IN LIST BEFORE --> SKIP THE STATE '''
             if spf.is_board_exist(new_board, list_state):
                 continue
-            ''' IF ONE OR MORE BOXES ARE STUCK IN THE CORNER --> SKIP THE STATE '''
+           
             if spf.is_board_can_not_win(new_board, list_check_point):
                 continue
-            ''' IF ALL BOXES ARE STUCK --> SKIP THE STATE '''
             if spf.is_all_boxes_stuck(new_board, list_check_point):
                 continue
 
-            ''' MAKE NEW STATE '''
             new_state = spf.state(new_board, now_state, list_check_point)
-            ''' CHECK WHETHER THE NEW STATE IS GOAL OR NOT '''
             if spf.check_win(new_board, list_check_point):
                 print("Found win")
                 return (new_state.get_line(), len(list_state))
             
-            ''' APPEND NEW STATE TO VISITED LIST AND TRAVERSED LIST '''
             list_state.append(new_state)
             list_visit.append(new_state)
 
-            ''' COMPUTE THE TIMEOUT '''
             end_time = time.time()
             if end_time - start_time > spf.TIME_OUT:
                 return []
         end_time = time.time()
         if end_time - start_time > spf.TIME_OUT:
             return []
-    ''' SOLUTION NOT FOUND '''
+
     print("Not Found")
     return []
