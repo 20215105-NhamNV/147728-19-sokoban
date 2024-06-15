2698abab64ade96ac6db90da8802fa9894b64795
unknown <phamdinhhai1672003@gmail.com>
2024-06-15 21:57:54 +0700

    Update BFS

--- Sokoban-main/Sources/bfs.py
+++ Sokoban-main/Sources/bfs.py
@@ -3,54 +3,35 @@ import time
 
 def BFS_search(board, list_check_point):
     start_time = time.time()
-    
-    if spf.check_win(board,list_check_point):
-        print("Found win")
-        return [board]
-    start_state = spf.state(board, None, list_check_point)
+    spf.spfInit(board,list_check_point)
+    board_ = spf.format(board)
+    start_state = spf.state(board_, None)
     list_state = [start_state]
     list_visit = [start_state]
     while len(list_visit) != 0:
-        
         now_state = list_visit.pop(0)
-        cur_pos = spf.find_position_player(now_state.board)
-
-        ''' 
-        time.sleep(1)
-        clear = lambda: os.system('cls')
-        clear()
-        print_matrix(now_state.board)
-        print("State visited : {}".format(len(list_state)))
-        print("State in queue : {}".format(len(list_visit)))
-        '''
-        
-
-        list_can_move = spf.get_next_pos(now_state.board, cur_pos)
-        for next_pos in list_can_move:
-            ''' MAKE NEW BOARD '''
-            new_board = spf.move(now_state.board, next_pos, cur_pos, list_check_point)
+        now_board = now_state.board
+        list_next_board = spf.get_next_boards(now_board)
+        for next_board in list_next_board:
+            new_board = next_board
+            if spf.is_board_can_not_win(new_board):
+                continue
             if spf.is_board_exist(new_board, list_state):
                 continue
-           
-            if spf.is_board_can_not_win(new_board, list_check_point):
-                continue
-            if spf.is_all_boxes_stuck(new_board, list_check_point):
-                continue
-
-            new_state = spf.state(new_board, now_state, list_check_point)
-            if spf.check_win(new_board, list_check_point):
+            new_state = spf.state(new_board, now_state)
+            if spf.check_win(new_board):
                 print("Found win")
-                return (new_state.get_line(), len(list_state))
-            
+                end_time = time.time()
+                print(end_time - start_time)
+                print(len(list_state))
+                return (new_state.get_line_(), len(list_state))
             list_state.append(new_state)
             list_visit.append(new_state)
-
             end_time = time.time()
             if end_time - start_time > spf.TIME_OUT:
                 return []
         end_time = time.time()
         if end_time - start_time > spf.TIME_OUT:
             return []
-
     print("Not Found")
     return []
